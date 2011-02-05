b66c25ae40d6f7248930fb7897d2ee931238fd2e
James Chuck <james.chuck@marketboomer.com>
2011-02-05 12:02:32 +1100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoodLuckBookClub::Application.config.secret_token = '3ec8eeb70b7f02f16cc0a2b0e393841057806c7de92bc8e7a94bcc7490e5ecefeb0a30dd6f22cd2a78091c8b602d239faf31bfdc88ec1add820d9a3a14e026aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GoodLuckBookClub::Application.config.session_store :cookie_store, :key => '_GoodLuckBookClub_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GoodLuckBookClub::Application.config.session_store :active_record_store
